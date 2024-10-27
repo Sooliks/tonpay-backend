b691f32573c2794d75ff42badbeae18e933f6339
Sooliks <evgenij.terexov.2000@mail.ru>
2024-10-27 21:00:53 +0300

    add orders

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Request } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, Request } from "@nestjs/common";
 import { OrdersService } from './orders.service';
 import { CreateFeedbackDto } from "../feedback/feedback.dto";
 import { CreateOrderDto } from "./orders.dto";
@@ -7,9 +7,22 @@ import { CreateOrderDto } from "./orders.dto";
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
-  @Post('create')
+  @Post()
   async createOrder(@Body() dto: CreateOrderDto, @Request() req){
     dto.userId = req.user.id;
     return this.ordersService.createOrder(dto)
+  }
+
+  @Get('byid/:id')
+  async getOrder(@Param('id') id: string) {
+
+  }
+  @Get('mysales')
+  async getMySales(@Request() req) {
+    return this.ordersService.getMySales(req.user.id)
+  }
+  @Get('mypurchases')
+  async getMyPurchases(@Request() req) {
+    return this.ordersService.getMyPurchases(req.user.id)
   }
 }
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -8,43 +8,55 @@ import { NotificationsService } from "../notifications/notifications.service";
 @Injectable()
 export class OrdersService {
     constructor(private readonly prisma: PrismaService, private readonly moneyService: MoneyService, private readonly chatService: ChatService, private readonly notificationsService: NotificationsService) {}
-    async createOrder(dto: CreateOrderDto){
-        const sale = await this.prisma.sale.findUnique({where: {id: dto.saleId}})
-        if(!sale){
+    async createOrder(dto: CreateOrderDto) {
+        const sale = await this.prisma.sale.findUnique({ where: { id: dto.saleId } })
+        if (!sale) {
             throw new NotFoundException("No sale found")
         }
-        if(sale.isModerating || !sale.isPublished){
+        if (sale.isModerating || !sale.isPublished) {
             throw new BadRequestException("No sale found")
         }
-        if(sale.userId === dto.userId){
+        if (sale.userId === dto.userId) {
             throw new BadRequestException("You can't buy your own product")
         }
         await this.moneyService.minusMoney(dto.userId, sale.price)
         const product: string | undefined = sale.product.at(-1);
-        if(product){
+        if (product) {
             const products = sale.product.splice(sale.product.length - 1, 1);
             const updatedSale = await this.prisma.sale.update({
-                where: {id: sale.id},
+                where: { id: sale.id },
                 data: {
                     product: { set: products }
                 }
             })
-            if(updatedSale.product.length === 0){
-                await this.prisma.sale.update({where: {id: sale.id}, data: {isPublished: false}})
+            if (updatedSale.product.length === 0) {
+                await this.prisma.sale.update({ where: { id: sale.id }, data: { isPublished: false } })
             }
         }
-        const message = await this.chatService.createMessage({recipientId: dto.userId, senderId: sale.userId, message: `The order has been created, confirm only after the order is fully completed.${product ? `\nAuto delivery: ${product}` : ''}`}, true)
+        const message = await this.chatService.createMessage({
+            recipientId: dto.userId,
+            senderId: sale.userId,
+            message: `The order has been created, confirm only after the order is fully completed.${product ? `\nAuto delivery: ${product}` : ''}`
+        }, true)
         await this.notificationsService.notifyUser(sale.userId, 'You have a new order', true)
         const order = this.prisma.order.create({
             data: {
                 saleId: sale.id,
-                customerId: dto.userId, amount: sale.price, product: product
+                customerId: dto.userId,
+                amount: sale.price,
+                product: product,
+                sellerId: sale.userId
             }
         })
         return {
             order: order,
             chatId: message.chatId
         }
-
+    }
+    async getMyPurchases(userId: string) {
+        return this.prisma.order.findMany({where: {customerId: userId}})
+    }
+    async getMySales(userId: string) {
+        return this.prisma.order.findMany({where: {sellerId: userId}})
     }
 }
